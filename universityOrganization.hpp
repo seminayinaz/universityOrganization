8405699dd8b68c74402a7cabf91b39c67802d82f
seminayinaz <seminay.inaz@gmail.com>
2021-05-01 02:34:32 +0300

    Update universityOrganization.hpp

--- universityOrganization.hpp
+++ universityOrganization.hpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
+#include <algorithm>
+#pragma once
 
 using namespace std;
 
@@ -16,9 +19,11 @@ class Course{
     public:
         Course(string semester, int ID, string syllabus, int numberOfHomeworks, int numberOfProjects);
         ~Course();
-
         void setSyllabus(string syllabus);
         void addPrerequisite(string prerequisite);
+        string getSemester(){
+            return semester;
+        }
         void printCourse(){
             cout << "semester: "  << semester << "\nID: " << ID << "\nNumber Of Homeworks: " << numberOfHomeworks ;
             cout << "\nNumber Of Projects: " << numberOfProjects << "\nPrerequisite Courses: " ;
@@ -26,5 +31,31 @@ class Course{
                 cout << prerequisite << ",";
             cout << "\n";
         }
+};
+typedef map<string,vector<Course>> CourseOffered;
+class Instructor{
+    private:
+        string fullName;
+        int ID;
+        int startingDate;
+        int department;
+        bool fullTime;
+        int journalPapers;
+        int conferencePapers;
+        string laboratoryName;
+        CourseOffered courseOffered;
+    public:
+        Instructor(string fullName,int ID,int startingDate,int department,bool isProfessor,string laboratoryName);
+        ~Instructor();
+        bool isProfessor();
+        void addCourse(Course course);
+        void removeCourse(string course);
+        void printCoursesOffered();
+        void printInstructor(){
+            cout << "Full Name: "  << fullName << "\nID: " << ID << "\nStarting Date: " << startingDate << "\nDepartment: " << department;
+            cout << "\nLaboratory Name: " << laboratoryName << "\nJournal Papers: " << journalPapers << "\nConference Papers: " << conferencePapers;
+            isProfessor() ? cout << "Professor\n" : cout << "not Professor\n";
+        }
+
 
 };
